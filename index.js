f2d8137853997bf5d539ebd1aad986ec8ff9e198
A-Abd <abrar.abdulwahed@gmail.com>
2023-01-01 12:36:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,23 +2,18 @@
     // take a callback function as an argument
     // call the callback function
     // it doesn't matter what this function returns, so long as it calls the callback function
-function receivesAFunction(spy){
-    spy();
-}
+const receivesAFunction = (cb) => cb();
 
 
 // returnsANamedFunction function should:
     // take no arguments
     // return a named function
-function returnsANamedFunction(){
-    return receivesAFunction;
-}
+const returnsANamedFunction = () => function namedFn(){};
+
 
 // The returnsAnAnonymousFunction function should:
 //     take no arguments
 //     return an anonymous function
-function returnsAnAnonymousFunction(){
-    return ()=>{}
-}
+const returnsAnAnonymousFunction = () => () =>{}
 
 
